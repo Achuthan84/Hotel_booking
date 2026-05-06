3c8cd68c723082fd1e20b7f2fa2cd2f994907ee4
Achuthan <achuthan302004@gmail.com>
2026-05-06 13:33:34 +0530

    issues

--- Server/middleware/uploadMiddleware.js
+++ Server/middleware/uploadMiddleware.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { files: 4, fileSize: 10 * 1024 * 1024 }, // 10MB per file
+  limits: { files: 4, fileSize: 10 * 1024 * 1024 },
 });
 
 export default upload;
